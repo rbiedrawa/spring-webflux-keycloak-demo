454ea5b821ca95c5b415e4b84dfdefef58ae2196
rbiedrawa <biedrawa.rafal@gmail.com>
2021-04-06 07:30:44 +0200

    Init commit

---
+++ src/test/java/com/rbiedrawa/oauth/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.rbiedrawa.oauth;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
